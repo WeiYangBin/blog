f55059e0fdfeb6cbb72e18416abe33c0dc9ab1e4
魏杨斌 <weiyangbin@weiyangbindeMacBook-Pro.local>
2019-09-01 14:18:31 +0800

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,24 +7,24 @@ module.exports = {
   description: 'About Me',
   dest: 'dist',
   themeConfig,
-  plugins: [
-    [
-      'vuepress-plugin-comment',
-      {
-        choosen: 'gitalk',
-        options: {
-          clientID: 'b97cdb14a8b4c92ed33c',
-          clientSecret: '8de8ec03e99dfae940ec6aa28dad88245a8c1eac',
-          repo: 'blog',
-          owner: 'YangBin Wei',
-          admin: ['YangBin Wei'],
-          id: '<%- frontmatter.commentid || frontmatter.title %>',
-          distractionFreeMode: false,  // Facebook-like distraction free mode
-          labels: ['Gitalk', 'Comment'],
-          title: '「评论」<%- frontmatter.title %>',
-          body: '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
-        }
-      }
-    ]
-  ]
+  // plugins: [
+  //   [
+  //     'vuepress-plugin-comment',
+  //     {
+  //       choosen: 'gitalk',
+  //       options: {
+  //         clientID: 'b97cdb14a8b4c92ed33c',
+  //         clientSecret: '8de8ec03e99dfae940ec6aa28dad88245a8c1eac',
+  //         repo: 'blog',
+  //         owner: 'YangBin Wei',
+  //         admin: ['YangBin Wei'],
+  //         id: '<%- frontmatter.commentid || frontmatter.title %>',
+  //         distractionFreeMode: false,  // Facebook-like distraction free mode
+  //         labels: ['Gitalk', 'Comment'],
+  //         title: '「评论」<%- frontmatter.title %>',
+  //         body: '<%- frontmatter.title %>：<%- window.location.origin %><%- frontmatter.to.path || window.location.pathname %>'
+  //       }
+  //     }
+  //   ]
+  // ]
 }
